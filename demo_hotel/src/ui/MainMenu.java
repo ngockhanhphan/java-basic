6d3e4e53c1820d5efaa09bdda973cbf62c0b10aa
Khanh Phan <phanngockhanh.as97@gmail.com>
2024-10-16 14:18:01 +0700

    Show main menu screen

--- demo_hotel/src/ui/MainMenu.java
+++ demo_hotel/src/ui/MainMenu.java
@@ -1,4 +1,27 @@
 package ui;
 
+import java.util.Scanner;
+
 public class MainMenu {
+    Scanner scanner = new Scanner(System.in);
+
+    public MainMenu(Scanner scanner) { this.scanner = scanner; }
+
+    public void mainMenu() {
+        showMainMenu();
+    }
+
+    public static void showMainMenu() {
+        System.out.print("""
+                Welcome to the Hotel Reservation Application!
+                ------------------------------------
+                1. Find and reserve a room.
+                2. See my reservations.
+                3. Create an Account.
+                4. Admin.
+                5. Exit.
+                ------------------------------------
+                Please select a number for the menu option:
+                """);
+    }
 }
