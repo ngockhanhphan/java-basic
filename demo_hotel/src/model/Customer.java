76e541d05e5ca8a415b94affe3c0c0fd87eddc4a
Khanh Phan <phanngockhanh.as97@gmail.com>
2024-10-16 14:06:33 +0700

    Create Customer model

--- demo_hotel/src/model/Customer.java
+++ demo_hotel/src/model/Customer.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Customer {
+    String firstName;
+    String lastName;
+    String email;
+
+    public Customer(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public String toString() {
+        return "First Name: " + this.firstName
+                + ", Last Name: " + this.lastName
+                + ", Email: " + this.email;
+    }
 }
